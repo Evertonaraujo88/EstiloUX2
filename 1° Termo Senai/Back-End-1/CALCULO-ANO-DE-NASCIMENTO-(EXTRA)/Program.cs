adefcb898efea1d96a5048ec86e3ec2b68990e02
Usuario <evertonaraujosenai@gmail.com>
2023-03-30 06:34:49 -0300

    atualizacao atividade extra

---
+++ 1° Termo Senai/Back-End-1/CALCULO-ANO-DE-NASCIMENTO-(EXTRA)/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine($"Digite o Ano de seu Nascimento");
+var Nascimento = (Console.ReadLine());
+
+var data = DateTime.Now;
+var formatada = String.Format("{0:yyyy}", data);
+
+var anos = ((formatada) - Nascimento);
+
+ 

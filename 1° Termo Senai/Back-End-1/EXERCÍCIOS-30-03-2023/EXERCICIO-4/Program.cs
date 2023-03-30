875fd60c241e4a605e57d788c59c438b0e9c959e
Everton Silva De Araujo <32599952859@info.local>
2023-03-30 18:24:29 -0300

    exercicio4

---
+++ 1° Termo Senai/Back-End-1/EXERCÍCIOS-30-03-2023/EXERCICIO-4/Program.cs
@@ -0,0 +1,16 @@
+﻿//4)Escreva     um     programa     que     verifique     a     validade     de     uma     senha     fornecida pelo     usuário. 
+//A     senha     válida     é     o     número     1234. 
+//Devem     ser    impressas     as    seguintes     mensagens:
+//ACESSO    PERMITIDO    caso    a    senha    seja    válida.
+//ACESSO    NEGADO    caso    a    senha    seja    inválida.
+Console.WriteLine($"Digite a senha:");
+int senha = int.Parse(Console.ReadLine());
+
+int correta = 1234;
+
+if (senha == correta){
+    Console.WriteLine($"ACESSO PERMITIDO!!");
+    
+}else {
+    Console.WriteLine($"ACESSO NEGADO!!");
+}

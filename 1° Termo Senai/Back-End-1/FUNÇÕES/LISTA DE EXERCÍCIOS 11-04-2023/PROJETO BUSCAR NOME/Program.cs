298fc05ef4a08928b1a71c152760f14dd3bb7cb0
Everton Silva De Araujo <32599952859@info.local>
2023-04-17 09:48:00 -0300

    exercicios

--- 1° Termo Senai/Back-End-1/FUNÇÕES/LISTA DE EXERCÍCIOS 11-04-2023/PROJETO BUSCAR NOME/Program.cs
+++ 1° Termo Senai/Back-End-1/FUNÇÕES/LISTA DE EXERCÍCIOS 11-04-2023/PROJETO BUSCAR NOME/Program.cs
@@ -3,14 +3,44 @@ lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nom
 pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
 entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário. */
 
-string [] ListaNomes = new string[10];
+bool encontrado = false;
+string [] listaNomes = new string[10];
 
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine($"Digite o nome da {i+1}° pessoas: ");
-    ListaNomes[i] = Console.ReadLine();
-    
-    
-
-    
+    listaNomes[i] = Console.ReadLine();
+        
 }
+
+
+ Console.WriteLine($"Digite o nome que deseja buscar:");
+ string busca = Console.ReadLine();
+ 
+
+ foreach (string nome in listaNomes)
+ {
+    if (nome == busca)
+    {
+        encontrado = true;
+        break;
+    }
+    
+ }
+ 
+/*  if (encontrado == true)
+{
+    Console.WriteLine($"ACHEIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!!!!!!!!!!!!");    
+}
+else
+{
+    Console.WriteLine($"NÃO  ACHEIIIIIIIIIIIIIIIIIIIIIIIII!");    
+} */
+
+/*string resultadoPesquisa = encontrado == true ? "ACHEI!!" : "NÃO ACHEI!!!";
+Console.WriteLine(resultadoPesquisa);*/
+
+Console.WriteLine(encontrado == true ? "ACHEI" : "NÃO ACHEI");
+   
+    
+    

bcfcce10eff4ed520d25b9593d13ac885a47dac6
Evertonaraujo88 <evertonaraujosenai@gmail.com>
2023-05-19 11:01:42 -0300

    exercicios produtos

--- 1° Termo Senai/Back-End-1/PROJETO-PRODUTOS/Classes/Marca.cs
+++ 1° Termo Senai/Back-End-1/PROJETO-PRODUTOS/Classes/Marca.cs
@@ -11,7 +11,7 @@ namespace PROJETO_PRODUTOS.Classes
        public string NomeMarca{ get; private set; }
        public DateTime DataCadastro{ get; private set; }
 
-       List<Marca> ListadeMarcas = new List<Marca>();
+       public static List<Marca> ListaDeMarcas = new List<Marca>();
 
        public Marca Cadastrar()
        {
@@ -20,26 +20,45 @@ namespace PROJETO_PRODUTOS.Classes
         Marca novaMarca = new Marca();
         
         Console.WriteLine($"Digite o Código da Marca:");
-        novaMarca.Codigo = int.Parse(Console.ReadLine());
-        
-        Console.WriteLine($"Digite o nome da Marca:");
-        novaMarca.NomeMarca = Console.ReadLine();
+        int codigo = int.Parse(Console.ReadLine());
 
-        novaMarca.DataCadastro = DateTime.Now;
+        novaMarca = ListaDeMarcas.Find(x=>x.Codigo == codigo);
+        //Console.WriteLine($"{novaMarca}");
+       
+       
+        if (novaMarca == null)
+        {
+            novaMarca = new Marca();
+            novaMarca.Codigo = codigo;
+
+            Console.WriteLine($"Digite o nome da Marca:");
+            novaMarca.NomeMarca = Console.ReadLine();
+
+            novaMarca.DataCadastro = DateTime.Now;
     
-        ListadeMarcas.Add(novaMarca);
-        
-        
-        return novaMarca;
+            ListaDeMarcas.Add(novaMarca);
+
+            return novaMarca;
+            
+        }
+        else
+        {
+
+            Console.WriteLine($"Marca já cadastrada!");
+
+            return novaMarca;
+            
+        }
+    
        }
 
        public void Listar()
        {
         //aqui vai a lógica
         //exibir os objetos cadastrados na lista
-        if (ListadeMarcas.Count > 0 || ListadeMarcas != null)
+        if (ListaDeMarcas.Count > 0 || ListaDeMarcas != null)
             {
-               foreach (Marca p in ListadeMarcas)
+               foreach (Marca p in ListaDeMarcas)
                {
                 Console.WriteLine(@$"
                 Código: {p.Codigo}
@@ -48,7 +67,7 @@ namespace PROJETO_PRODUTOS.Classes
                 ");
                 
                 }
-            }if ( ListadeMarcas.Count < 1)
+            }if ( ListaDeMarcas.Count < 1)
             {
                 Console.WriteLine($"Não exitem marcas cadastrados");   
             }
@@ -59,14 +78,14 @@ namespace PROJETO_PRODUTOS.Classes
        {
         //aqui vai a lógica
         //buscar um objeto na lista pelo seu código
-        int index = ListadeMarcas.IndexOf(ListadeMarcas.Find(x=>x.Codigo == _codigo));
+        int index = ListaDeMarcas.IndexOf(ListaDeMarcas.Find(x=>x.Codigo == _codigo));
         //remove-lo
         Console.WriteLine($"Deseja exluir o Produto? s para sim e n para não");
             string resp = Console.ReadLine();
             
             if (resp == "s")
             {
-                ListadeMarcas.RemoveAt(index);
+                ListaDeMarcas.RemoveAt(index);
                 Console.WriteLine($"Marca Excluída com sucesso!");
             }
        }

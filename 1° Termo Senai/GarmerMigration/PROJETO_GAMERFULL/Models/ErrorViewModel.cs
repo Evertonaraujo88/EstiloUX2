6fca26f54ec58a62ccb33bfb4b65356893236118
Evertonaraujo88 <evertonaraujosenai@gmail.com>
2023-06-20 10:33:37 -0300

    finalizacao do projeto Gamer

--- 1° Termo Senai/GarmerMigration/PROJETO_GAMERFULL/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PROJETO_GAMERFULL.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}

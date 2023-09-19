a18c510d87efe6fa99b4ca5dc332825eff2d0c59
EVertonaraujo88 <evertonaraujosenai@gmail.com>
2023-09-19 10:51:39 -0300

    event+_manha feito..criado pasta context, controler Usuario, interfaces, utils criptografia, repositories usuarioRepository

--- 2º Termo Senai/Banco de dados/Event+_codeFirst/webapi.event+.manha/Domains/PresencasEventoDomain.cs
+++ 2º Termo Senai/Banco de dados/Event+_codeFirst/webapi.event+.manha/Domains/PresencasEventoDomain.cs
@@ -3,30 +3,30 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace webapi.event_.manha.Domains
 {
-    [Table (nameof(PresencaEventoDomain)) ]
-    public class PresencaEventoDomain
+    [Table (nameof(PresencasEventoDomain)) ]
+    public class PresencasEventoDomain
     {
 
         [Key]
-        public Guid IdPresenca { get; set; } = Guid.NewGuid();
+        public Guid IdPresencaEvento { get; set; } = Guid.NewGuid();
 
-        [Column (TypeName ="Bool")]
-        [Required(ErrorMessage ="É obrigatorio confirmar presença!")]
+        [Column (TypeName ="BIT")]
+        [Required(ErrorMessage ="É obrigatorio confirmar ou não a presença!")]
         public bool Situacao { get; set; }
 
         //Refenrência a tabela Usuario = FK
-        [Required(ErrorMessage ="O usuario eh obrigatorio!")]
+        [Required(ErrorMessage ="O usuario é obrigatorio!")]
         public Guid IdUsuario { get; set; }
 
         [ForeignKey(nameof(IdUsuario))]
         public UsuarioDomain? Usuario { get; set; }
 
-        //Ref. tabela Evento
+        //Ref. tabela Evento = Fk
         [Required(ErrorMessage = "É obrigatório Informar o evento!")]
         public Guid IdEvento { get; set; }
 
         [ForeignKey(nameof(IdEvento))]
-        public UsuarioDomain? Evento { get; set; }
+        public EventoDomain? Evento { get; set; }
     }
 
 

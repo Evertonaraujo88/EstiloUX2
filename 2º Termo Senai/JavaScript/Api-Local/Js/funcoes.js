03e8cfb63edffd8323040457947bb0dc1d2d139c
EVertonaraujo88 <evertonaraujosenai@gmail.com>
2023-10-20 10:54:06 -0300

    intruducao javaScript e ecmaScript e api

---
+++ 2º Termo Senai/JavaScript/Api-Local/Js/funcoes.js
@@ -0,0 +1,34 @@
+const urlViaCep = "https://viacep.com.br/ws";
+
+function cadastrar(e) {
+    e.preventDefault();
+   /*  alert("Cadastrar..."); */
+
+}
+
+async function buscarEndereco(cep) {
+    /* complemento do endereco da api */
+    const resource = `/${cep}/json/`;
+
+    try {
+
+        const promise = await fetch(urlViaCep + resource);
+
+        /* transformo o json retornado em objeto oui array */
+        const endereco = await promise.json();
+        console.log(endereco);
+        
+        /* acessa os dados o bjeto endereço(viacep) e tras os values(valores) do objeto para o form de acordo com os ids definidos nos inputs  */
+        document.getElementById("logradouro").value = endereco.logradouro;
+        document.getElementById("localidade").value = endereco.localidade;
+        document.getElementById("uf").value = endereco.uf;
+
+        /* resetar o span de cep inválido */
+        document.getElementById("not-found").innerText = "";
+    } catch (error) {
+
+        document.getElementById("not-found").innerText = "CEP Inválido";
+    }
+
+
+}

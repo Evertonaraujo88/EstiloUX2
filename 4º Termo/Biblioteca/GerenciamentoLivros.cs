4b941c30b93c3a0fd081306ed8bc64843f1e8fdd
EvertonAraujo88 <evertonaraujosenai@gmail.com>
2024-08-01 23:26:48 -0300

    finalizando exercicios de testes unitarios

--- 4º Termo/Biblioteca/GerenciamentoLivros.cs
+++ 4º Termo/Biblioteca/GerenciamentoLivros.cs
@@ -6,9 +6,26 @@ using System.Threading.Tasks;
 
 namespace Biblioteca
 {
-    public static class GerenciamentoLivros
-    {
 
+    
+   
+        // Classe estática que gerencia a lista de livros
+        public static class GerenciamentoLivros
+        {
+            // Lista privada de livros
+            private static List<Livro> _livros = new List<Livro>();
 
-    }
+            // Método para adicionar um livro à lista
+            public static void AdicionarLivro(Livro livro)
+            {
+                _livros.Add(livro);
+            }
+
+            // Método para obter a lista de livros
+            public static List<Livro> ObterLivros()
+            {
+                return _livros;
+            }
+        }
+    
 }

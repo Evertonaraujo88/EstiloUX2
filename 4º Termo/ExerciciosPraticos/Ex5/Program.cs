3dc1f194895d18938afc5db73b704d6c25bc76f5
Evertonaraujo88 <evertonaraujosenai@gmail.com>
2024-08-12 08:06:11 -0300

    finalização lista de exercicios de revisão

--- 4º Termo/ExerciciosPraticos/Ex5/Program.cs
+++ 4º Termo/ExerciciosPraticos/Ex5/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Crie um programa que peça ao usuário para digitar um texto e conte quantas vezes cada letra do alfabeto aparece no texto.
+
+
+        Console.Write("Digite um texto: ");
+        string texto = Console.ReadLine().ToLower();
+
+        // instanciando Dicionário para armazenar a contagem de cada letra
+        Dictionary<char, int> contagemLetras = new Dictionary<char, int>();
+
+  
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            contagemLetras[c] = 0;
+        }
+
+        foreach (char c in texto)
+        {
+            if (contagemLetras.ContainsKey(c))
+            {
+                contagemLetras[c]++;
+            }
+        }
+
+
+        Console.WriteLine("\nContagem de letras no texto:");
+        foreach (var item in contagemLetras)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+
